942279fd31b3b18ba5425490cf3a944f6083ca8b
Janani <jananivarshini2003@gmail.com>
2026-03-13 06:30:14 +0530

    project update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import aboutCSS from './../About/About.module.css'
+import aboutImg from './../../assets/about-one.avif'
 function About(){
     return(
-        <div className="section">
-            <h1>About</h1>
+        <div className={`${aboutCSS.about_wraper} section`}>
+           <div className={aboutCSS.about_img}>
+            <img src={aboutImg} alt="about-img" />
+           </div>
+           <div className={aboutCSS.about_content}>
+            <small className="section-Heading">vibeNoW Events</small>
+            <h2 className="section_Title">Turning Your Special Moments into <span>Beautiful Memories</span></h2>
+            <div className={aboutCSS.Cards}>
+                <p>260+ <span>Award Wins</span></p>
+                 <p>250k+ <span>Visitors Visits</span></p>
+                  <p>150k+<span>Events</span></p>
+
+            </div>
+           </div>
         </div>
     )
 }
